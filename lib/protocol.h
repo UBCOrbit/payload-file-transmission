7a2277f55606a3c647f85d9a288a5e85a2412bdc
Justin Frank <justinpfrank@protonmail.com>
2018-02-03 14:55:37 -0800

    changed protocol from enum to defines

--- lib/protocol.h
+++ lib/protocol.h
@@ -4,14 +4,12 @@
 // 2^15 bytes, 32 kb
 #define PACKET_SIZE 0x8000
 
-enum TransferCommands {
-    TRANSFER_START = 1,
-    TRANSFER_PACKET,
-    TRANSFER_NEXT,
-    TRANSFER_AGAIN,
-    TRANSFER_END,
-    TRANSFER_ERROR
-};
+#define TRANSFER_START  1
+#define TRANSFER_PACKET 2
+#define TRANSFER_NEXT   3
+#define TRANSFER_AGAIN  4
+#define TRANSFER_END    5
+#define TRANSFER_ERROR  6
 
 #endif // protocol_h_INCLUDED
 
